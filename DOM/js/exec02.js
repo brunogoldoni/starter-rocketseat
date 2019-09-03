d6fd9c583678020674d37a08659548e269f07135
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-09-03 11:07:18 -0300

    Renomeando pasta

---
+++ DOM/js/exec02.js
@@ -0,0 +1,9 @@
+function pares (x, y) {
+  for (i = x; i <= y; i++) {
+    if (i % 2 === 0) {
+      console.log(i)
+    }
+  }
+}
+
+pares(32, 321)
---
+++ DOM/js/exec04.js
@@ -0,0 +1,15 @@
+function experiencia(anos) {
+  if (anos <= 1) {
+    return "Iniciante"
+  } else if (anos <= 3) {
+    return "Intermediário"
+  } else if (anos <= 6) {
+    return "Avançado"
+  } else {
+    return "Jedi Master"
+  }
+}
+
+let anosEstudos = 7;
+
+console.log(experiencia(anosEstudos));
