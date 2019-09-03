55a3d898f8b1e4e68e522fcc6dcc35fcd82090e2
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-09-03 17:06:08 -0300

    Valores Padrão

--- EcmaScript/bundle.js
+++ EcmaScript/bundle.js
@@ -1,15 +1,1 @@
 "use strict";
-
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var newArr = arr.map(function (item) {
-  return item * 2;
-});
-console.log(newArr);
-
-var test = function test() {
-  return {
-    name: 'Bruno'
-  };
-};
-
-console.log(test());
