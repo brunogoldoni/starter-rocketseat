04ac45ba8dd4635a9d0f0f000e4ed6750036f2cf
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-09-04 17:50:50 -0300

    Async - Await

--- EcmaScript/src/main.js
+++ EcmaScript/src/main.js
@@ -1,1 +1,12 @@
-alert('Ops...')
+const myPromise = () => new Promise((resolve, reject) => {
+  setTimeout(() => { resolve('OK') }, 2000);
+});
+
+async function executePromisse() {
+
+  console.log(await myPromise());
+  console.log(await myPromise());
+  console.log(await myPromise());
+}
+
+executePromisse();
