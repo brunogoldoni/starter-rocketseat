5c79e12ce54cb4d34b36d6b0f1c613dc94194ddb
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-09-04 09:44:52 -0300

    Arquitetura de pastas
    -Alterando arquitetura de pasta

---
+++ EcmaScript/introdução/template-literals.js
@@ -0,0 +1,5 @@
+//Tempalte Literals - incluindo variaveis dentro de strings apartir do ES6
+const name = 'Bruno';
+const age = 31;
+
+console.log(`Meu nome é ${name} e tenho ${age} anos.`);
---
+++ EcmaScript/introdução/valores-padrao.js
@@ -0,0 +1,4 @@
+const sum = (a = 3, b = 7) => a + b;
+
+console.log(sum(1));
+console.log(sum());
