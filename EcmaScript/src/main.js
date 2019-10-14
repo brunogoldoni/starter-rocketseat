53492b6bfd3100039c32de2f27b2c4b3f65d5a44
Bruno Mendonça Goldoni <bgoldoni@orion.tech>
2019-10-14 18:41:47 -0300

    Aplicação
    -Buscando valores da api

--- EcmaScript/src/main.js
+++ EcmaScript/src/main.js
@@ -1,8 +1,11 @@
+import api from './api';
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
+    this.inputEl = document.querySelector('input[name=repository]')
     this.listEl = document.getElementById('repo-list');
 
     this.registerHandlers();
@@ -13,15 +16,26 @@ class App {
 
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length == 0) {
+    }
+
+    const response = await api.get(`/repos/${repoInput}`)
+
+    const { name, description, html_url, owner: { avatar_url }} = response.data;
+
     this.repositories.push({
-      name: 'Rocketseat.com.br',
-      description: 'Tire a sua ideia do papel e dê vida á sua startup',
-      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
-      html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
+
+    this.inputEl.value = '';
 
     this.render()
   }
@@ -40,6 +54,7 @@ class App {
 
       let linkEl = document.createElement('a');
       linkEl.setAttribute('target', '_blank');
+      linkEl.setAttribute('href', repo.html_url);
       linkEl.appendChild(document.createTextNode('Acessar'));
 
       let listItemEl = document.createElement('li');
